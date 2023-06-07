be8e2d52491005145a3bd1e68aaa3e3ca2e35c5d
pietro <pietro@go.go>
2023-06-06 22:51:51 -0300

    teste dict

--- QLearning/BattleZone/main.py
+++ QLearning/BattleZone/main.py
@@ -2,7 +2,7 @@ import gymnasium as gym
 import numpy as np
 import collections
 
-env = gym.make("ALE/BattleZone-v5", render_mode="human", obs_type="ram")
+env = gym.make("ALE/BattleZone-v5", render_mode=None, obs_type="ram")
 
 actions = env.action_space.n
 
@@ -11,15 +11,18 @@ qTable = collections.defaultdict(float)
 gamma = 0.4
 lr = 0.1
 
-"""def conf(state):
-    o = 0
-    for b in state:
-        for x in b:
-            o = (o << 1) & x
-    return o"""
+def convert(n):
+  out = 0
+  for bit in n:
+    out = (out << 1) | bit
+  return out
+
 
 def NextQ(qTable, state):
-    return np.max(qTable[state, :])
+    qv = []
+    for a in range(actions):
+        qv.append(qTable[state + (a, )])
+    return max(qv)
 
 
 stats = []
@@ -28,8 +31,17 @@ for i in range(eps):
     totalEpReward = 0
     state = tuple(env.reset()[0])
     
+    
     x = True
     while x:
+        qv = []
+        for i in range(actions):
+            v = qTable[state + (i,)]
+            if v > 0.0:
+                print(v)
+            qv.append(qTable[state + (i,)])
+        #print(qv)
+
         action = env.action_space.sample()
         nstate, reward, done, trunc, i = env.step(action)
         nstate = tuple(nstate)
@@ -37,7 +49,14 @@ for i in range(eps):
 
         cstate = state + (action,)
         #print(cstate)
-        qTable[cstate] = (1 - lr) * qTable[state, action] + gamma * (reward - qTable[state, action])
+        qTable[cstate] = qTable[state, action] + lr * (reward + gamma * NextQ(qTable,nstate) - qTable[state, action])
+        #v = qTable[cstate]
+
+        state = nstate
+        if done:
+            break
+        if trunc:
+            break
 
     stats.append((i,totalEpReward))
-    print(qTable)
+    #print(qTable)
