a3e9f5d4024bdba801b196884bf978c15402eaee
pietro <pietro@go.go>
2023-06-08 01:06:12 -0300

    organizando o codigo

--- QLearning/BattleZone/main.py
+++ QLearning/BattleZone/main.py
@@ -3,20 +3,17 @@ import numpy as np
 import collections
 import random
 import pickle
+import argparse
 
-env = gym.make("ALE/BattleZone-v5", render_mode="human", obs_type="ram")
+parser = argparse.ArgumentParser()
+parser.add_argument("--mode",   default=None,   help="Render mode: human/None")
+parser.add_argument("--epoch",  default=10,     help="Iterations.", type=int)
+parser.add_argument("--load",   default="dump.ql")
+parser.add_argument("--save",   default="dump.ql", help="File to save the model.")
+parser.add_argument("--randact",default=0.5, help="Random action.")
+parser.add_argument("--dict",   default="disk", help="Dict ram/disk.")
+args = parser.parse_args()
 
-actions = env.action_space.n
-
-#qTable = collections.defaultdict(float)
-hand = open("./dump.ql","rb")
-qTable = pickle.load(hand)
-hand.close()
-
-eps = 1
-gamma = 0.4
-lr = 0.1
-randAct = 0.0
 
 def converter(n):
   out = 0
@@ -25,47 +22,70 @@ def converter(n):
   return out
 
 
-def NextQ(qTable, state):
+def NextQ(qTable, state, actions):
     qv = []
     for a in range(actions):
         qv.append(qTable[(state,a)])
     return max(qv)
 
+def main(eps,gamma,lr,randAct,qTable,env,actions):
+    stats = []
+    doneTax = 0
+    truncTax = 0
 
-stats = []
+    for i in range(0,eps):
+        totalEpReward = 0
+        state = converter(env.reset()[0])
+        action = 0
+        x = True
+        while x:
+            if random.uniform(0,1) > randAct:
+                qv = []
+                for j in range(actions):
+                    qv.append(qTable[(state,j)])
+                action = np.argmax(qv)
+            else:
+                action = env.action_space.sample()
 
-for i in range(eps):
-    totalEpReward = 0
-    state = converter(env.reset()[0])
-    action = 0
-    x = True
-    while x:
-        if random.uniform(0,1) > randAct:
-            qv = []
-            for i in range(actions):
-                qv.append(qTable[(state,i)])
-            action = np.argmax(qv)
-        else:
-            action = env.action_space.sample()
+            nstate, reward, done, trunc, info = env.step(action)
+            nextState = converter(nstate)
+            
+            cstate = (state,action)
+            qTable[cstate] = qTable[cstate] + lr * (reward + gamma * NextQ(qTable,nextState,actions) - qTable[cstate])
 
-        nstate, reward, done, trunc, i = env.step(action)
-        nextState = converter(nstate)
-        
-        cstate = (state,action)
-        print(cstate)
-        qTable[cstate] = qTable[cstate] + lr * (reward + gamma * NextQ(qTable,nextState) - qTable[cstate])
+            totalEpReward += reward
+            state = nextState
+            if done:
+                doneTax+=1
+                break
+            if trunc:
+                truncTax+=1
+                break
+        stats.append((i,totalEpReward,doneTax,truncTax))
+        #if i%10 == 0:
+        print("Iteration:",i," Total Rewards:",totalEpReward," Done:",doneTax," Trunc:",truncTax)
+    return qTable
 
-        totalEpReward += reward
-        state = nextState
-        if done:
-            break
-        if trunc:
-            break
+env = gym.make("ALE/BattleZone-v5", render_mode=args.mode, obs_type="ram")
+actions = env.action_space.n
 
+qTable = None
+if args.dict == "disk":
+    hand = open(args.load,"rb")
+    qTable = pickle.load(hand)
+    hand.close()
+elif args.dict == "ram":
+    print("Problems opening file model, loading in memory training.")
+    qTable = collections.defaultdict(float)
+else:
+    exit(1)
 
+eps = args.epoch
+gamma = 0.4
+lr = 0.1
+randAct = 0.1
 
-    stats.append((i,totalEpReward))
-   #print(qTable)
-f = open("./dump.ql","wb")
-pickle.dump(qTable,f)
+qt = main(eps,gamma,lr,randAct,qTable,env,actions)
+f = open(args.save,"wb")
+pickle.dump(qt,f)
 f.close()
